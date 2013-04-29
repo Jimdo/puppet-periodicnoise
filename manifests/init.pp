b5882d1fd1fe87bb5da1f04c333ed521b08af7a8
Ingo Oeser <ingo@jimdo.com>
2013-04-29 18:37:41 +0200

    fixup requirements

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,10 @@
 # Install/configure something here
 class periodicnoise($send_as_host = undef, $send_to_host = undef) {
   package{ 'periodicnoise': ensure => present }
+  file { '/etc/periodicnoise': ensure => directory}
   file { '/etc/periodicnoise/config.ini' :
     ensure  => present,
     content => template('periodicnoise/nsca.erb'),
+    require => Package['periodicnoise'],
   }
 }
--- spec/classes/periodicnoise_spec.rb
+++ spec/classes/periodicnoise_spec.rb
@@ -12,7 +12,10 @@ describe 'periodicnoise' do
   end
 
   it 'should configure periodicnoise globally for NSCA helpers in oder to see failures in nagios' do
+    should contain_file('/etc/periodicnoise').with_ensure('directory')
     should contain_file('/etc/periodicnoise/config.ini')\
+      .with_ensure('present') \
+      .with_require('Package[periodicnoise]') \
       .with_content(%r{send_nsca}) \
       .with_content(%r{somehost.example.com}) \
       .with_content(%r{monitor.example.com})
