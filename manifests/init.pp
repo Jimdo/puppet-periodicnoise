4e7f59c6c2420377979e8cb2aa491f9d0d06a620
Ingo Oeser <ingo@jimdo.com>
2013-04-29 17:25:14 +0200

    actually pass parameters

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,5 @@
 # Install/configure something here
-class periodicnoise(send_as_host, send_to_host) {
+class periodicnoise($send_as_host, $send_to_host) {
   $send_as_host = $send_as_host
   $send_to_host = $send_to_host
   package{ 'periodicnoise': ensure => present }
