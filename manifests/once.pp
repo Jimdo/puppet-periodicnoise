61808ac25963460c0a9aa1b4b2d543e570978e0d
Ingo Oeser <ingo@jimdo.com>
2013-06-27 15:29:06 +0200

    provide mechanism for once only jobs
    Sometimes you don't want to wait for a cron,
    but trigger something as soon, as puppet runs
    and still use all the guarding/wrapping of periodicnoise.
    
    Provide periodicnoise::once for this use case.

---
+++ manifests/once.pp
@@ -0,0 +1,36 @@
+define periodicnoise::once (
+  $command,
+  $execution_timeout,
+  $user                      = undef,
+  $creates                   = undef,
+  $onlyif                    = undef,
+  $refresh                   = undef,
+  $refreshonly               = undef,
+  $tries                     = undef,
+  $try_sleep                 = undef,
+  $unless                    = undef,
+  $max_execution_start_delay = undef,
+  $kill_running_instance     = undef,
+  $disable_stdout_log        = undef,
+  $disable_stderr_log        = undef,
+  $use_syslog                = undef,
+  $wrap_nagios_plugin        = undef,
+  $event                     = undef
+) {
+  include periodicnoise::params
+
+  # Variables used in cron.erb
+  $_max_execution_start_delay = $max_execution_start_delay ? { undef => $periodicnoise::params::pn_max_execution_start_delay, default => $max_execution_start_delay }
+  $_execution_timeout         = $execution_timeout
+  $_kill_running_instance     = $kill_running_instance ? { undef => $periodicnoise::params::pn_kill_running_instance, default => $kill_running_instance }
+  $_disable_stdout_log        = $disable_stdout_log ? { undef => $periodicnoise::params::pn_disable_stdout_log, default => $disable_stdout_log }
+  $_disable_stderr_log        = $disable_stderr_log ? { undef => $periodicnoise::params::pn_disable_stderr_log, default => $disable_stderr_log }
+  $_use_syslog                = $use_syslog ? { undef => $periodicnoise::params::pn_use_syslog, default => $use_syslog }
+  $_wrap_nagios_plugin        = $wrap_nagios_plugin ? { undef => $periodicnoise::params::pn_wrap_nagios_plugin, default => $wrap_nagios_plugin }
+
+  exec { $name:
+    command   => template('periodicnoise/cron.erb'),
+    user      => $user ? { undef => $periodicnoise::params::cron_user, default => $user },
+    require   => Package['periodicnoise']
+  }
+}
---
+++ spec/defines/once_spec.rb
@@ -0,0 +1,141 @@
+require 'spec_helper'
+
+describe 'periodicnoise::once', :type => :define do
+
+  let (:title) { 'some_oncejob' }
+  let (:params) {{
+    :command           => 'some_once_command',
+    :user              => 'root',
+    :execution_timeout => '10m'
+  }}
+
+  it "should create a oncejob" do
+    should contain_exec('some_oncejob') \
+      .with_command('pn --timeout=10m --use-syslog -- some_once_command') \
+      .with_user('root') \
+      .with_require('Package[periodicnoise]')
+  end
+
+  context "with event set" do
+    let (:params) {{
+      :command           => 'some_once_command',
+      :user              => 'root',
+      :event             => 'some_event',
+      :execution_timeout => '10m'
+    }}
+    it "should create a oncejob with event set" do
+      should contain_exec('some_oncejob') \
+        .with_command('pn --monitor-event=\'some_event\' --timeout=10m --use-syslog -- some_once_command')
+    end
+  end
+
+  context "with stderr logging disabled" do
+    let (:params) {{
+      :command            => 'some_once_command',
+      :user               => 'root',
+      :disable_stderr_log => true,
+      :execution_timeout  => '10m'
+    }}
+    it "should create a oncejob with stderr logging disabled" do
+      should contain_exec('some_oncejob') \
+        .with_command('pn --timeout=10m --no-stream-stderr --use-syslog -- some_once_command')
+    end
+  end
+
+  context "with maximum execution start delay set to 2 minutes" do
+    let (:params) {{
+      :command                   => 'some_once_command',
+      :user                      => 'root',
+      :max_execution_start_delay => '2m',
+      :execution_timeout         => '10m'
+    }}
+    it "should create a oncejob with maximum execution start delay set to 2 minutes" do
+      should contain_exec('some_oncejob') \
+        .with_command('pn --max-start-delay=2m --timeout=10m --use-syslog -- some_once_command')
+    end
+  end
+
+  context "with kill already running instance enabled" do
+    let (:params) {{
+      :command               => 'some_once_command',
+      :user                  => 'root',
+      :kill_running_instance => true,
+      :execution_timeout     => '10m'
+    }}
+    it "should create a oncejob with kill already running instance enabled" do
+      should contain_exec('some_oncejob') \
+        .with_command('pn --timeout=10m --kill-running --use-syslog -- some_once_command')
+    end
+  end
+
+  context "with disabled stdout logging" do
+    let (:params) {{
+      :command            => 'some_once_command',
+      :user               => 'root',
+      :disable_stdout_log => true,
+      :execution_timeout  => '10m'
+    }}
+    it "should create a oncejob with disabled stdout logging" do
+      should contain_exec('some_oncejob') \
+        .with_command('pn --timeout=10m --no-stream-stdout --use-syslog -- some_once_command')
+    end
+  end
+
+  context "syslog disabled" do
+    let (:params) {{
+      :command           => 'some_once_command',
+      :user              => 'root',
+      :use_syslog        => false,
+      :execution_timeout => '10m'
+    }}
+    it "should create a oncejob with syslog logging enabled" do
+      should contain_exec('some_oncejob') \
+        .with_command('pn --timeout=10m -- some_once_command')
+    end
+  end
+
+  context "with execution timeout set to 2 minutes" do
+    let (:params) {{
+      :command            => 'some_once_command',
+      :user               => 'root',
+      :execution_timeout  => '2m'
+    }}
+    it "should create a oncejob with execution timeout set to 2 minutes" do
+      should contain_exec('some_oncejob') \
+        .with_command('pn --timeout=2m --use-syslog -- some_once_command')
+    end
+  end
+
+  context "with wrap nagios plugin enabled" do
+    let (:params) {{
+      :command            => 'some_once_command',
+      :user               => 'root',
+      :wrap_nagios_plugin => true,
+      :execution_timeout  => '10m'
+    }}
+    it "should create a oncejob with execution timeout set to 2 minutes" do
+      should contain_exec('some_oncejob') \
+        .with_command('pn --timeout=10m --use-syslog --wrap-nagios-plugin -- some_once_command')
+    end
+  end
+
+  context "with all params enabled" do
+    let (:params) {{
+      :command                    => 'some_once_command',
+      :user                       => 'root',
+      :event                      => 'some_event',
+      :disable_stderr_log         => true,
+      :max_execution_start_delay  => '2m',
+      :kill_running_instance      => true,
+      :disable_stdout_log         => true,
+      :use_syslog                 => true,
+      :execution_timeout          => '2m',
+      :wrap_nagios_plugin         => true
+    }}
+    it "should create a oncejob with event set" do
+      should contain_exec('some_oncejob') \
+        .with_command('pn --monitor-event=\'some_event\' --max-start-delay=2m --timeout=2m --kill-running --no-stream-stdout --no-stream-stderr --use-syslog --wrap-nagios-plugin -- some_once_command')
+    end
+  end
+
+end
